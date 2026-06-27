4d888811a160fe57c38dfad9e93a48c4b55e0df6
Git_Winfrise <>
2026-06-27 17:57:01 +0800

    把dpi改为300

--- src/pdf_to_images.py
+++ src/pdf_to_images.py
@@ -47,5 +47,5 @@ def pdf_to_images(pdf_path, output_folder=None, dpi=72, img_format="jpg"):
 # --- 使用示例 ---
 if __name__ == "__main__":
     # 替换为你的PDF路径
-    pdf_file = "/Users/teacher/Desktop/未命名文件夹 2/2.5氟碳漆铝单板-金奥维.pdf" 
-    pdf_to_images(pdf_file, dpi=72)
+    pdf_file = "/Users/teacher/Desktop/未命名文件夹 3/思凡尼2026图册.pdf" 
+    pdf_to_images(pdf_file, dpi=300)
